374606efbdbafc664fef45d8533cc6deea7865da
dranih <drani@ik.me>
2021-10-17 17:50:13 +0200

    Initialize

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dranih/go-crud-api/pkg/handlers/apiserver"
+)
+
+func main() {
+	apiserver.Handle()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dranih/go-crud-api
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
