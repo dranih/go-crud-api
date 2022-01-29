538249d09985d86bfae900cab8429691e0f5c0fc
dranih <drani@ik.me>
2022-01-29 19:10:32 +0100

    Script for starting all types of test db in a container

--- test/README.md
+++ test/README.md
@@ -1,6 +1,6 @@
 # Testing
 * The folder **sql** contains the sql files to create the test database for sqlite, mysql, pgsql and sqlserver. These are copies from php-crud-api repo test folder.
 * The folder **yaml** contains the gocrudapi config files to test with each type of database
-* The bash script **start_db.sh** <mysql|pgsql|sqlserver> starts a database with docker of the choosen type and execute the sql file to it. The GCA_CONFIG_FILE has to be set to the adequat yaml file before running the tests.
+* The bash script **start_db.sh** <mysql|pgsql|sqlsrv|all> [clean] starts a database with docker of the choosen type and execute the sql file to it. The GCA_CONFIG_FILE has to be set to the adequat yaml file before running the tests.
 
 By default (if GCA_CONFIG_FILE env var is empty), the tests which need a database will load a sqlite database into **/tmp/gocrudtests.db**
--- test/start_db.sh
+++ test/start_db.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 function usage {
-    echo "Usage : start_db.sh <mysql|pgsql|sqlserver> [clean]"
+    echo "Usage : start_db.sh <mysql|pgsql|sqlsrv|all> [clean]"
 }
 
 function clean {
     #Remove container if exists
     if [ $( docker ps -a -f name=${containerName} | wc -l ) -eq 2 ]; then
+        echo "Deleting container ${containerName}"
         docker rm -f ${containerName}
     fi
 }
@@ -14,7 +15,7 @@ function clean {
 function test {
     code=$1
     msg=$2
-    if [[ $? != 0 ]]; then
+    if [ ${code} != 0 ]; then
         echo $msg
         exit $code
     fi
@@ -26,13 +27,67 @@ function started {
     echo "export GCA_CONFIG_FILE=${configFile}"
 }
 
+function isContainerRunning {
+    [ z$( docker ps -q -f name=${containerName} ) != z ]
+}
+
+function startAndLoad {
+    case ${dbType} in
+        mysql)
+            dbPort=3306
+            startContainerCmd="-e MYSQL_ALLOW_EMPTY_PASSWORD=true -d mysql:latest"
+            createDbCmd="mysql -uroot < /gocrud/create_mysql.sql"
+            loadSqlCmd="mysql go-crud-api -ugo-crud-api -pgo-crud-api < /gocrud/blog_mysql.sql"
+            ;;
+        pgsql)
+            dbPort=5432
+            startContainerCmd="-e POSTGRES_PASSWORD=go-crud-api -d postgis/postgis"
+            createDbCmd="PGPASSWORD=go-crud-api psql -q -U postgres -f /gocrud/create_pgsql.sql"
+            loadSqlCmd="PGPASSWORD=go-crud-api psql -q -U go-crud-api -f /gocrud/blog_pgsql.sql"
+            ;;
+        sqlsrv)
+            dbPort=1433
+            startContainerCmd="-e ACCEPT_EULA=Y -e SA_PASSWORD=mystrongPasw0rd! -d mcr.microsoft.com/mssql/server:2019-latest"
+            createDbCmd="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P mystrongPasw0rd! -i /gocrud/create_sqlsrv.sql"
+            loadSqlCmd="/opt/mssql-tools/bin/sqlcmd -S localhost -U go-crud-api -P go-crud-api -i /gocrud/blog_sqlsrv.sql"
+            ;;
+        *)
+            echo "This should not happen"
+            exit 254
+            ;;
+    esac
+
+    echo ">>>>> ${dbType}"
+    containerName="gocrud_${dbType}"
+    configFile="${scriptDir}/yaml/gcaconfig_${dbType}.yaml"
+    if [[ "${action}" != "clean" ]]; then
+        if ! isContainerRunning ; then
+            clean
+            echo "Starting container ${containerName}"
+            docker run -p ${dbPort}:${dbPort} --name ${containerName} -v ${scriptDir}/sql:/gocrud ${startContainerCmd}
+            #Wait for db to start, should do it cleaner
+            sleep 15
+            docker exec ${containerName} bash -c "${createDbCmd}"
+            test $? "Could not create database"
+        else
+            echo "Container ${containerName} already running"
+        fi
+        docker exec ${containerName} bash -c "${loadSqlCmd}"
+        test $? "Could not load test sql in database"
+        started
+    else
+        clean
+    fi
+}
+
+# Start
 if (! docker stats --no-stream > /dev/null 2>&1); then
     echo "Is docker running ?"
     exit 254
 fi
 
 dbType=$1
-clean=$2
+action=$2
 scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [[ z"${dbType}" == z ]]; then
@@ -40,26 +95,15 @@ if [[ z"${dbType}" == z ]]; then
     exit 254
 fi
 
-containerName="gocrud_${dbType}"
-configFile="${scriptDir}/yaml/gcaconfig_${dbType}.yaml"
-
 case ${dbType} in
-  mysql)
-    clean
-    if [[ "${clean}" != "clean" ]]; then
-        docker run -p 3306:3306 --name ${containerName} -v ${scriptDir}/sql:/gocrud -e MYSQL_ALLOW_EMPTY_PASSWORD=true -d mysql:latest
-        #Wait for db to start, should do it cleaner
-        sleep 15
-        docker exec ${containerName} bash -c "mysql -uroot < /gocrud/create_mysql.sql"
-        test $? "Could not create database"
-        docker exec ${containerName} bash -c "mysql go-crud-api -ugo-crud-api -pgo-crud-api < /gocrud/blog_mysql.sql"
-        test $? "Could not load test sql in database"
-        started
-    fi
+  mysql | pgsql | sqlsrv)
+    startAndLoad
     ;;
-  pgsql)
-    ;;
-  sqlserver)
+  all)
+    for dbType in mysql pgsql sqlsrv
+    do
+        startAndLoad
+    done
     ;;
   *)
     usage
